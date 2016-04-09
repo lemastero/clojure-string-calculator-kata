81a03781201ae0a4c1f51c70e42cf97150d17576
lemastero <adres.mistrza@gmail.com>
2016-04-09 14:48:38 +0200

    Initial project generated by Leiningen: lein new string-calculator

---
+++ src/string_calculator/core.clj
@@ -0,0 +1,6 @@
+(ns string-calculator.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/string_calculator/core_test.clj
@@ -0,0 +1,6 @@
+(ns string-calculator.core-test
+  (:require [clojure.test :refer :all]
+            [string-calculator.core :refer :all]))
+
+
+
